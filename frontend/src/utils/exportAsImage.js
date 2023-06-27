3e2a1a19ce59941f2f1f40fa648ede6d6eb4fdb1
= <=>
2023-06-27 17:42:32 +0200

    added firebase hosting

--- frontend/src/utils/exportAsImage.js
+++ frontend/src/utils/exportAsImage.js
@@ -1,6 +1,6 @@
 import html2canvas from "html2canvas";
 
-const exportAsImage = async (element, imageFileName) => {
+const exportAsImage = async (element, callback) => {
     const canvas = await html2canvas(element, {
         allowTaint: false,
         useCORS: true,
@@ -8,22 +8,25 @@ const exportAsImage = async (element, imageFileName) => {
         scrollX: 0,
         scrollY: -window.scrollY,
     });
-    const image = canvas.toDataURL("image/png", 1.0);
-    downloadImage(image, imageFileName);
+    //const image = canvas.toDataURL("image/png", 1.0);
+    //downloadImage(image, imageFileName);
+    canvas.toBlob(blob => {
+        callback(blob);
+    });
 };
 
-const downloadImage = (blob, fileName) => {
-  const fakeLink = window.document.createElement("a");
-  fakeLink.style = "display:none;";
-  fakeLink.download = fileName;
+/*const downloadImage = (blob, fileName) => {
+    const fakeLink = window.document.createElement("a");
+    fakeLink.style = "display:none;";
+    fakeLink.download = fileName;
 
-  fakeLink.href = blob;
+    fakeLink.href = blob;
 
-  document.body.appendChild(fakeLink);
-  fakeLink.click();
-  document.body.removeChild(fakeLink);
+    document.body.appendChild(fakeLink);
+    fakeLink.click();
+    document.body.removeChild(fakeLink);
 
-  fakeLink.remove();
-};
+    fakeLink.remove();
+};*/
 
 export default exportAsImage;
--- smartcontract/contracts/GreenTextNFT.sol
+++ smartcontract/contracts/GreenTextNFT.sol
@@ -1,32 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /* For test */
 //import "hardhat/console.sol";
 
-contract GreenTextNFT is ERC721, ERC721Enumerable {
+contract GreenTextNFT is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIdCounter;
-
-    //address private _nativeToken;
     address payable private _adminAccount;
     uint256 private _mintFee;
+    string private _baseURL;
 
-    constructor(/*address nativeToken, */address adminAccount) ERC721("GreenText", "GreenText") {
-        //_nativeToken = nativeToken;
+    constructor(address adminAccount) ERC721("GreenText", "GreenText") {
         _adminAccount = payable(adminAccount);
         _mintFee = 3 * 10 ** 15;
+        _baseURL = "https://localhost/green-text-nft/";
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://localhost/green-text-nft/";
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURL;
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseURL = baseURI;
+    }
+
+    function setAdminAccount(address adminAccount) external onlyOwner {
+        _adminAccount = payable(adminAccount);
     }
 
     function mint() public payable {
@@ -34,33 +40,26 @@ contract GreenTextNFT is ERC721, ERC721Enumerable {
         //console.log("Sender's Balance:", sender.balance);
         //console.log("Sented Fee:", msg.value);
         //console.log("Required Fee:", _mintFee);
-        require(_adminAccount != sender, "Not user");
         require(msg.value >= _mintFee, "Insufficient Fee");
 
-        //uint256 balance = IERC20(_nativeToken).balanceOf(to);
-        //require(balance >= _mintFee, "Insufficent fee");
-        //if (IERC20(_nativeToken).transferFrom(to, _adminAccount, _mintFee))
-
         //console.log("Transfering %s from %s to %s...", _mintFee, sender, _adminAccount);
-        if (_adminAccount.send(_mintFee))
+        if (_adminAccount.send(msg.value))
         {
-            if (msg.value > _mintFee)
-                sender.transfer(msg.value - _mintFee);
+            //if (msg.value > _mintFee)
+            //    sender.transfer(msg.value - _mintFee);
             
             uint256 tokenId = _tokenIdCounter.current();
             //console.log("Minting token:", tokenId);
             _tokenIdCounter.increment();
             _safeMint(sender, tokenId);
         }
-        //else
-            //console.log("Failed to transfer native currency");
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
+    /*function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
-    }
+    }*/
 }
